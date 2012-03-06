4a235af0d765a717c40a10d05984af8ab68ae78a
Michael.Kamal@gmail.com <Michael.Kamal@gmail.com@fe1c5792-4ad0-72e2-a41c-25ee34ea83fa>
2012-03-06 14:47:45 +0000

    finished the Manual Lexical Analyzer java with no output tested .. both .. mageds place

--- trunk/compiler/src/Lexer/Lexer.java
+++ trunk/compiler/src/Lexer/Lexer.java
@@ -1,4 +1,5 @@
 package Lexer;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -52,11 +53,24 @@ public class Lexer {
 		return false;
 	}
 
+	// Checks if a character is a character
+	private boolean isChar(char c) {
+		if ((c >= 'a' && c <= 'z') || (c >= 'a' && c <= 'z'))
+			return true;
+
+		return false;
+	}
+
 	public Token nextToken() {
 
 		int state = 1; // Initial state
-		int numBuffer = 0; // A buffer for number literals
+		int decimalPoint = 0;
+		int intBuffer = 0;
+		float numBuffer = 0; // A buffer for number literals
 		String symbolBuffer = "";
+		String charBuffer = "";
+		String stringBuffer = "";
+		String stringErrorBuffer = "";
 
 		while (true) {
 			if (curr == EOF) {
@@ -73,6 +87,9 @@ public class Lexer {
 			case 1:
 				switch (curr) {
 				case ' ': // Whitespaces
+					curr = read();
+					continue;
+				
 				case '\n':
 				case '\b':
 				case '\t':
@@ -80,11 +97,11 @@ public class Lexer {
 				case '\f':
 					curr = read();
 					continue;
-				
+					
 				case ';':
 					curr = read();
 					return new Token("SM", ";");
-					
+
 				case ',':
 					curr = read();
 					return new Token("FA", ",");
@@ -92,16 +109,16 @@ public class Lexer {
 				case '(':
 					curr = read();
 					return new Token("LP", "(");
-				
+
 				case ')':
 					curr = read();
 					return new Token("RP", ")");
-				
+
 				case '{':
-				
+
 					curr = read();
 					return new Token("LB", "{");
-				
+
 				case '}':
 					curr = read();
 					return new Token("RB", "}");
@@ -120,26 +137,34 @@ public class Lexer {
 
 				case '/':
 					curr = read();
-					return new Token("DO", "/");
-
+					if(curr == '*'){
+						state = 6;
+						curr = read();
+						continue;
+					}else if(curr == '/'){
+						state = 7;
+						curr = read();
+						continue;
+					}else return new Token("DO", "/");
 				case '%':
 					curr = read();
 					return new Token("MD", "%");
-					
+
 				case '=':
-					if(symbolBuffer == "="){
+					if (symbolBuffer == "=") {
 						curr = read();
 						return new Token("EQ", "==");
-					}else if(symbolBuffer == "!"){
+					} else if (symbolBuffer == "!") {
 						curr = read();
 						return new Token("NE", "!=");
-					}else{
+					} else {
 						symbolBuffer += curr;
 						curr = read();
-						if(curr != '='){
+						if (curr != '=') {
 							symbolBuffer = "";
 							return new Token("AO", "=");
-						}else continue;
+						} else
+							continue;
 					}
 				case '!':
 					symbolBuffer += curr;
@@ -147,52 +172,134 @@ public class Lexer {
 					continue;
 				case '|':
 					curr = read();
-					if(curr == '|'){
+					if (curr == '|') {
 						curr = read();
 						return new Token("LO", "||");
 					}
 					continue;
 				case '&':
 					curr = read();
-					if(curr == '&'){
+					if (curr == '&') {
 						curr = read();
 						return new Token("LA", "&&");
 					}
 					continue;
-					
-					//look ahead
-					
+				case '"':
+					stringBuffer = "";
+					stringBuffer += curr;
+					state = 4;
+					curr = read();
+					continue;
+
+					// look ahead
 
 				default:
 					state = 2; // Check the next possibility
 					continue;
 				}
 
-				// Integer - Start
+				// char - Start
 			case 2:
-				if (isNumeric(curr)) {
-					numBuffer = 0; // Reset the buffer.
-					numBuffer += (curr - '0');
+				if (isChar(curr)  || curr == '_') {
+					charBuffer = ""; // Reset the buffer.
+					charBuffer += curr;
 
 					state = 3;
 					curr = read();
-				} else {
+				} else if (isNumeric(curr)) {
+					intBuffer = 0; // Reset the buffer.
+					intBuffer += (curr - '0');
+					
+					state = 5;
+					curr = read();
+				}else {
 					return null;
 				}
 				continue;
-
-				// Integer - Body
 			case 3:
-				if (isNumeric(curr)) {
-					numBuffer *= 10;
-					numBuffer += (curr - '0');
+				if (isChar(curr) || isNumeric(curr) || curr == '_') {
+					charBuffer += curr;
 
 					curr = read();
 				} else {
+					if (charBuffer.equals("class") || charBuffer.equals("else")
+							|| charBuffer.equals("if")
+							|| charBuffer.equals("int")
+							|| charBuffer.equals("float")
+							|| charBuffer.equals("boolean")
+							|| charBuffer.equals("String")
+							|| charBuffer.equals("return")
+							|| charBuffer.equals("static")
+							|| charBuffer.equals("while"))
+						return new Token("KW", "" + charBuffer);
+					else if(charBuffer.equals("true") || charBuffer.equals("false"))
+						return new Token("BL", "" + charBuffer);
+					else  return new Token("ID", "" + charBuffer);
+					
+				}
+				continue;
+			case 4:
+				if (curr == '\\') {
+					stringBuffer += curr;
+					stringErrorBuffer = "";
+					stringErrorBuffer += curr;
+					curr = read();
+					if(curr == 'n' || curr == 'r'){
+						stringErrorBuffer += curr;
+						//return new Token("ERROR", "" + stringErrorBuffer); // next entrance in the method will enter state 1
+					}
+				}
+				if (curr != '"') {
+					stringBuffer += curr;
+					curr = read();
+				}else {
+					return new Token("ST", "" + stringBuffer);
+				}
+				continue;
+				// number - Start
+			case 5:
+				if (!isNumeric(curr) || curr != '.'){
+					//return new Token("ERROR", "" + numBuffer); //next entrance will be
+				}
+				if (curr == '.'){
+					numBuffer = 0;
+					numBuffer += intBuffer;
+					decimalPoint = 10;
+				}
+				if (isNumeric(curr) && decimalPoint < 10) {
+					intBuffer *= 10; // Reset the buffer.
+					intBuffer += (curr-0);
+					curr = read();
+				}else if(isNumeric(curr)){
+					numBuffer += ((curr-0)/decimalPoint);
+					decimalPoint *= 10;
+					curr = read();
+				}else if(decimalPoint < 10){
+					return new Token("NUM", "" + intBuffer);
+				}else {
 					return new Token("NUM", "" + numBuffer);
 				}
 				continue;
+				
+			//multiline comments
+			case 6:
+				if (curr == '*'){
+					curr = read();
+					if(curr == '/'){
+						state = 1;
+					}
+				}
+				curr = read();
+				continue;
+			//singleline comments
+			case 7:
+				if (curr == '\n'){
+					state = 1;
+				}
+				curr = read();
+				continue;
 			}
+			
 		}
 	}
 }
