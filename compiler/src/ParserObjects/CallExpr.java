a22e7360b11d3ec40693ce9373e5bc31d7c901e7
Michael.Kamal <Michael.Kamal@gmail.com>
2012-05-10 14:58:54 +0000

    finished callexp

--- compiler/src/ParserObjects/CallExpr.java
+++ compiler/src/ParserObjects/CallExpr.java
@@ -38,10 +38,26 @@ public class CallExpr extends PrimaryExpr {
 		Entry e = symTable.get(id);
 		MethodDecl md = (MethodDecl) e.object;
 		returnType = md.t.type;
-		
 		for(int i = 0; i < md.getParamatersTypes().size(); i++){
-			if(!md.getParamatersTypes().get(i).equals(aps.getParamatersTypes().get(i))){
-				System.out.println(md.getParamatersTypes().get(i) + " " + aps.getParamatersTypes().get(i));
+			String t = aps.getParamatersTypes().get(i);
+			if(!t.equals("int") 
+				&& !t.equals("float") 
+				&& !t.equals("boolean") 
+				&& !t.equals("String")){
+				
+				Entry e1 = symTable.get(aps.getParamatersTypes().get(i));
+				
+				try {
+					FormalParam o = (FormalParam) e1.object;
+					t = o.t.type;
+				} catch (Exception e2) {
+					LocalVarDecl o = (LocalVarDecl) e1.object;
+					t = o.t.type;
+				}
+				aps.getParamatersTypes().set(i, t);
+				
+			}
+			if(!md.getParamatersTypes().get(i).equals(t)){
 				throw new SemanticException("Method \"" + id + "\" with the same Parameter types is not declared in the current scope");
 			}
 				
